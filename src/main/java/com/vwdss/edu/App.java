2b9ed85b50ca410de0e4409c51fab3ab073cde1c
Alexander Melnikov <amelnikov@melnikov.office.devexperts.com>
2016-06-22 10:49:12 +0200

    converted to a maven project

---
+++ src/main/java/com/vwdss/edu/App.java
@@ -0,0 +1,13 @@
+package com.vwdss.edu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
